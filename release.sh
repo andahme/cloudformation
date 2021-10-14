c4b67d248db683c804cc98e2a01488f7ce10693f
Andy Daniels <andy@andah.me>
2021-10-13 23:39:49 -0500

    Update release script, template descriptions.

--- release.sh
+++ release.sh
@@ -1,55 +1,50 @@
 #!/bin/bash
 
 
-export RELEASE_VERSION=$(cat version.txt | cut -d- -f1)
+export RELEASE_VERSION=${RELEASE_VERSION:=$(cat version.txt | cut -d- -f1)}
 export MAJOR_VERSION=$(echo ${RELEASE_VERSION} | cut -d. -f1)
 export MINOR_VERSION=$(echo ${RELEASE_VERSION} | cut -d. -f2)
 export PATCH_VERSION=$(echo ${RELEASE_VERSION} | cut -d. -f3)
 
-export POST_RELEASE_VERSION=${MAJOR_VERSION}.${MINOR_VERSION}.$((${PATCH_VERSION} + 1))-SNAPSHOT
+export NEXT_VERSION=${MAJOR_VERSION}.${MINOR_VERSION}.$((${PATCH_VERSION} + 1))
 
 
 # STATUS
 echo RELEASE_VERSION: ${RELEASE_VERSION}
-echo POST_RELEASE_VERSION: ${POST_RELEASE_VERSION}
+echo NEXT_VERSION: ${NEXT_VERSION}
 
 
 ###
-# PRE RELEASE
+# RELEASE PROCESS
 ###
 
 # UPDATE VERSION
 echo ${RELEASE_VERSION} > version.txt
-git add version.txt
-git commit -m "Release (v${RELEASE_VERSION})."
+find cloudformation -name "*.template" -exec sed -i '' "s/(v.*-SNAPSHOT)/(v${RELEASE_VERSION})/g" {} \;
 
-# TAG VERSION
+# COMMIT AND TAG
+git add version.txt $(find cloudformation -name "*.template")
+git commit -m "Release (v${RELEASE_VERSION})."
 git tag v${RELEASE_VERSION}
 
-
-
-###
 # CREATE RELEASE ARTIFACT
-###
-
-# BUILD
-find cloudformation -name "*.template" -exec sed -i '' s/SNAPSHOT/v${RELEASE_VERSION}/g {} \;
-
-# PACKAGE
-tar -zcf aws-stacker-${RELEASE_VERSION}.tar.gz cloudformation
+tar -zcf andahme-cloudformation-v${RELEASE_VERSION}.tar.gz cloudformation
 
 # CLEANUP
-git checkout -- cloudformation/.
-
+git reset HEAD~1
+git checkout -- .
 
 
 ###
-# POST RELEASE
+# POST RELEASE PROCESS
 ###
 
 # UPDATE VERSION
-echo ${POST_RELEASE_VERSION} > version.txt
-git add version.txt
-git commit -m "Post release version.txt update."
+echo ${NEXT_VERSION}-SNAPSHOT > version.txt
+find cloudformation -name "*.template" -exec sed -i '' "s/(v.*-SNAPSHOT)/(v${NEXT_VERSION}-SNAPSHOT)/g" {} \;
+
+# COMMIT
+git add version.txt $(find cloudformation -name "*.template")
+git commit -m "Post release version.txt update (${NEXT_VERSION}-SNAPSHOT)."
 
 
